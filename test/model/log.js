9354af986268ce7f0e3444ac31ecd2a6f0492dfd
Jamie Gaehring <gaehj457@gmail.com>
2021-03-28 17:05:04 -0400

    Add merge & setLastSync methods.

--- test/model/log.js
+++ test/model/log.js
@@ -80,4 +80,41 @@ describe('log', () => {
       }, 10);
     });
   });
+  describe('#merge', () => {
+    it('merges a remote log with no conflicts', (done) => {
+      const props = { type: 'activity', name: 'local log' };
+      const local = farm.log.create(props);
+      setTimeout(() => {
+        farm.meta.setLastSync(local);
+        const remote = farm.log.serialize(local);
+        remote.attributes.name = 'remote log';
+        remote.attributes.changed = new Date().toISOString();
+        delete remote.meta;
+        setTimeout(() => {
+          farm.log.merge(local, remote);
+          expect(local.name).to.equal('remote log');
+          const { conflicts } = farm.meta.get(local).fields.name;
+          expect(conflicts.length).to.equal(0);
+          done();
+        }, 10);
+      }, 10);
+    });
+    it('merges a remote, non-Drupal log with no conflicts', (done) => {
+      const props = { type: 'activity', name: 'local log' };
+      const local = farm.log.create(props);
+      setTimeout(() => {
+        farm.meta.setLastSync(local);
+        const remote = farm.log.serialize(local);
+        remote.attributes.name = 'remote log';
+        remote.meta.fields.name.changed = new Date().toISOString();
+        setTimeout(() => {
+          farm.log.merge(local, remote);
+          expect(local.name).to.equal('remote log');
+          const { conflicts } = farm.meta.get(local).fields.name;
+          expect(conflicts.length).to.equal(0);
+          done();
+        }, 10);
+      }, 10);
+    });
+  });
 });
