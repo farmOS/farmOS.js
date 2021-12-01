646ebf84109f5dd1a0f8f69442f7120104d7e77f
Jamie Gaehring <gaehj457@gmail.com>
2021-12-01 00:00:52 -0500

    Add type annotations to farmOS constructor.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,50 @@ import entities, { entityMethods } from './entities.js';
 
 const shortNames = Object.values(entities).map(e => e.shortName);
 
+/**
+ * @typedef {Object} FarmEntityMethods
+ * @property {import('./model/create.js').CreateEntityMethod} create
+ * @property {import('./model/update.js').UpdateEntityMethod} update
+ * @property {import('./model/merge.js').MergeEntityMethod} merge
+ * @property {import('./client/fetch.js').FetchEntityMethod} fetch
+ * @property {import('./client/send.js').SendEntityMethod} send
+ * @property {import('./client/delete.js').DeleteEntityMethod} delete
+ */
+
+/**
+ * @typedef {Object} FarmRemote
+ * @property {import('axios').AxiosInstance} request
+ * @property {Function} info
+ * @property {Function} authorize
+ * @property {Function} setHost
+ * @property {Function} getToken
+ */
+
+/**
+ * @typedef {Object} FarmInstance
+ * @property {Object} schema
+ * @property {Function} schema.get
+ * @property {Function} schema.set
+ * @property {Function} schema.on
+ * @property {Object} meta
+ * @property {Function} meta.isUnsynced
+ * @property {FarmRemote} remote
+ * @property {FarmEntityMethods} asset
+ * @property {FarmEntityMethods} log
+ * @property {FarmEntityMethods} plan
+ * @property {FarmEntityMethods} quantity
+ * @property {FarmEntityMethods} term
+ * @property {FarmEntityMethods} user
+ */
+
+/**
+ * @typedef {Function} FarmOS
+ * @param {Object} config
+ * @property {import('./model/index.js').EntitySchemata} [config.schemata]
+ * @property {Object} [config.remote]
+ * @property {import('./client/index.js').FarmClient} [config.remote.adapter]
+ * @returns {FarmInstance}
+ */
 export default function farmOS(config) {
   const {
     schemata = {},
