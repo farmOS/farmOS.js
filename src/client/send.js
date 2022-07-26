052bdf9e470fd8a8c5462110d072bf388212309d
Jamie Gaehring <gaehj457@gmail.com>
2022-07-26 15:16:12 -0400

    Refactor how sendEntity handles  PATCH 404's.

--- src/client/send.js
+++ src/client/send.js
@@ -9,33 +9,22 @@
 /**
  * @param {String} entityName
  * @param {Function} request
- * @param {String} bundle
- * @returns {(entity: String) => Promise}
- */
-const postEntity = (entityName, request, bundle) => data =>
-  request(`/api/${entityName}/${bundle}`, { method: 'POST', data });
-
-/**
- * @param {String} entityName
- * @param {Function} request
  * @returns {sendEntity}
  */
 const sendEntity = (entityName, request) => (bundle, entity) => {
   const data = JSON.stringify({ data: entity });
-  const post = postEntity(entityName, request, bundle);
+  const postURL = `/api/${entityName}/${bundle}`;
+  const postOptions = { method: 'POST', data };
+  if (!entity.id) return request(postURL, postOptions);
   // We assume if an entity has an id it is a PATCH request, but that may not be
   // the case if it has a client-generated id. Such a PATCH request will result
   // in a 404 (NOT FOUND), since the endpoint includes the id. We handle this
   // error with a POST request, but otherwise return a rejected promise.
-  if ('id' in entity) {
-    const patchURL = `/api/${entityName}/${bundle}/${entity.id}`;
-    const patchOptions = { method: 'PATCH', data };
-    const patchRequest = request(patchURL, patchOptions);
-    const intercept404 = error =>
-      (error.response && error.response.status === 404 ? post(data) : Promise.reject(error));
-    return patchRequest.catch(intercept404);
-  }
-  return post(data);
+  const patchURL = `/api/${entityName}/${bundle}/${entity.id}`;
+  const patchOptions = { method: 'PATCH', data };
+  const is404 = error => error.response && error.response.status === 404;
+  return request(patchURL, patchOptions)
+    .catch(e => (is404(e) ? request(postURL, postOptions) : Promise.reject(e)));
 };
 
 export default sendEntity;
