83f88f5f6738940aff12be428d85d6e1d96c33ab
Jamie Gaehring <gaehj457@gmail.com>
2022-09-01 15:16:01 -0400

    Refactor subrequests.

--- src/client/subrequests.js
+++ src/client/subrequests.js
@@ -5,21 +5,23 @@ import chain from 'ramda/src/chain';
 import compose from 'ramda/src/compose';
 import drop from 'ramda/src/drop';
 import evolve from 'ramda/src/evolve';
-import head from 'ramda/src/head';
+import filter from 'ramda/src/filter';
 import map from 'ramda/src/map';
+import mapObjIndexed from 'ramda/src/mapObjIndexed';
 import match from 'ramda/src/match';
-import mergeLeft from 'ramda/src/mergeLeft';
-import mergeWith from 'ramda/src/mergeWith';
+import mergeRight from 'ramda/src/mergeRight';
 import partition from 'ramda/src/partition';
 import path from 'ramda/src/path';
 import pick from 'ramda/src/pick';
 import prop from 'ramda/src/prop';
+import sort from 'ramda/src/sort';
 import startsWith from 'ramda/src/startsWith';
 import uniqBy from 'ramda/src/uniqBy';
 import { generateFieldTransforms, transformLocalEntity } from './adapter/transformations';
 
 // Constants for sending requests via Drupal's JSON:API module.
-const SUB_URL = '/api/subrequests?_format=json';
+const BASE_URI = '/api';
+const SUB_URL = `${BASE_URI}/subrequests?_format=json`;
 const headers = {
   Accept: 'application/vnd.api+json',
   'Content-Type': 'application/vnd.api+json',
@@ -42,7 +44,7 @@ function parseEntityType(fields) {
 
 // Subrequests that contain a JSONPath wildcard may have multiple subresponses,
 // which are each given Content-Ids comprised of the original requestId appended
-// w/ an additional fragment, which is itself comprised of the blueprint property
+// w/ an additional fragment, which is itself comprised of the blueprint key
 // where the wildcard was found plus an index. For instance, if a subrequest is
 // given a uri of '/api/log/input/{{find-logs.body@$.data[*].id}}' and has a
 // requestId of 'update-input', the first two subresponse that corresponds to it
@@ -52,12 +54,31 @@ function parseEntityType(fields) {
 const contentIdRE = /([^#\s]+)(#(body|uri)\{(\d)\})?$/;
 function parseContentId(string) {
   const [
-    contentId, requestId, fragment, property, index,
+    contentId, requestId, fragment, key, index,
   ] = match(contentIdRE, string);
   return {
-    contentId, requestId, fragment, property, index,
+    contentId, requestId, fragment, key, index,
   };
 }
+
+// Merge incoming subresponses w/ their original request data & prior subresponses.
+const concatSubresponses = (prior, requests) => compose(
+  // Merge right with prior, so the newest results replace any stale data.
+  mergeRight(prior),
+  // Map over subresponses, parsing & merging each with its request object.
+  mapObjIndexed(compose(
+    // Parse the body of each subresponse.
+    evolve({ body: JSON.parse }),
+    // Merge the request data with the response data.
+    sub => mergeRight(requests[sub.requestId], sub),
+    // Parse the contentId to get the original requestId, key and index, then
+    // merge those properties with the rest of the subresponse.
+    (sub, contentId) => mergeRight(parseContentId(contentId), sub),
+  )),
+  // The data object on the main response object contains the subresponses,
+  // each keyed to their contentId.
+  prop('data'),
+);
 
 // Determine if a relationship for a given schema is one-to-one (ie, 'object')
 // or one-to-many (ie, 'array').
@@ -70,7 +91,7 @@ export default function makeSendWithSubrequest(farm) {
   // Called before any requests have been sent, unlike resolveDependencies. This
   // is mutually recursive with parseSubrequests, and is how each subrequest and
   // its child subrequests are assigned their priority number, which accordingly
-  // determines in which batch of subrequests each is sent to the server.
+  // determines the batch of subrequests in which it will be sent to the server.
   function parseDependentFields(fields, action, prefix) {
     const [dependentFields, constants] = splitFields(fields);
     const { bundle, entity, type } = parseEntityType(constants);
@@ -93,7 +114,7 @@ export default function makeSendWithSubrequest(farm) {
 
   // This is called by blueprint functions just before the subrequest is batched
   // and actually sent to the server. This way, dependencies from earlier batches
-  // of requests can be resolved to their actual values, and concurrent dependencies
+  // of requests can be resolved to static values, and concurrent dependencies
   // (ie, dependencies that will be part of the same batch) can be suitably
   // formatted according to the JSONPath spec.
   function resolveDependencies(fieldData, ready, prior) {
@@ -104,7 +125,10 @@ export default function makeSendWithSubrequest(farm) {
 
     // Resolved fields include data from prior request batches, as well as concurrent
     // requests that do not require a post hoc subrequest.
-    const resolved = {}; const unresolved = [];
+    const resolved = {};
+    // The names of unresolved fields will be collected, but are ultimately left
+    // to the blueprint functions to handle independently.
+    const unresolved = [];
     // Post hoc subrequests are concurrent blueprints for creating relationships
     // that must be added to the entity separately from the main 'create'
     // subrequest, using the entity's relationship endpoint. This is likely b/c
@@ -115,55 +139,35 @@ export default function makeSendWithSubrequest(farm) {
     // and is not the sole responsibility of the actions.
     const waitFor = [];
 
-    // Returns the blueprint for a post hoc subrequest to create a one-to-many
-    // relationship that depends on a concurrent subrequest.
-    function posthocBlueprint(dependentRequestId, dependentField) {
-      const uuid = `{{${requestId}.body@$.data.id}}`;
-      const blueprint = {
-        requestId: `${requestId}.${dependentField}`,
-        // Note the entity's relationship endpoint is used here.
-        uri: `/api/${entity}/${bundle}/${uuid}/relationships/${dependentField}`,
-        waitFor: [requestId],
-        action: 'create',
-        headers,
-        body: JSON.stringify({
-          data: [{
-            id: `{{${dependentRequestId}.body@$.data[*].id}}`,
-            type: path([dependentRequestId, 'type'], ready),
-          }],
-        }),
-      };
-      return blueprint;
-    }
-
-    // Helpers for resolving resource identifiers from prior requests.
-    const pickResourceIdentifier = pick(['id', 'type']);
-    const findPriorData = id => path([id, 'data'], prior);
-    // For one-to-one relationships.
-    const takeFirstResolved = compose(
-      pickResourceIdentifier,
-      findPriorData,
-      head,
-    );
-    // For one-to-many relationships.
-    const concatAllResolved = compose(
+    // The contentId is included in each response object, so just use the array.
+    const responses = Object.values(prior);
+    // Match a requestId with its corresponding prior responses, if any exist.
+    const matchWithResponses = reqId => filter(sub => reqId === sub.requestId, responses);
+    // Field dependencies, as an array of raw requestIds, are mapped to an array
+    // of corresponding resource identifiers from prior responses.
+    const mapDependenciesToResources = compose(
+      // Take only the first occurrence of the resource, which will be from the
+      // most recent request since they've already been sorted by priority.
       uniqBy(prop('id')),
-      chain(pickResourceIdentifier),
-      map(findPriorData),
+      // Strip it down to the resource identifier alone.
+      map(pick(['id', 'type'])),
+      // Flatten the array of nested data objects/arrays.
+      chain(prop('data')),
+      // Responses are sorted by highest priority, which corresponds to the most recent.
+      // In the future it may also be useful to parse the requestId as a tie-breaker.
+      sort((a, b) => a.priority - b.priority),
+      // The array of requestIds, are each flatmapped to an array of matching responses.
+      chain(matchWithResponses),
     );
 
     // Iterate over all dependencies, resolving their corresponding fields using
     // the most up-to-date (highest priority) data available.
     Object.entries(dependencies).forEach(([field, fieldDeps]) => {
-      // 'object' === ONE-TO-ONE; 'array' === ONE-TO-MANY;
+      // Based on JSON Schema types, a field has a relation type of 'object' for
+      // ONE-TO-ONE relationships, or 'array' for ONE-TO-MANY relationships.
       const relation = typeOfRelationship(schema, field);
-      const concurrentIds = Object.keys(ready).filter(fieldDeps.includes);
-      const isResolved = compose(fieldDeps.includes, prop('requestId'), parseContentId);
-      const resolvedIds = Object.keys(prior).filter(isResolved);
-      // Sort by highest priority, which will also be the most recent. It may also
-      // be useful in the future to sort by parsing the requestId as a tie-breaker,
-      // but for now it's simpler to take one more-or-less at random.
-      resolvedIds.sort((a, b) => prior[a].priority - prior[b].priority);
+      const concurrentIds = Object.keys(ready).filter(id => fieldDeps.includes(id));
+      const resources = mapDependenciesToResources(fieldDeps);
       // ONE-TO-ONE RELATIONSHIPS
       if (relation === 'object') {
         // Concurrent dependencies are taken first b/c they will be the most recent.
@@ -175,23 +179,43 @@ export default function makeSendWithSubrequest(farm) {
           // all others can be resolved or, in the case of one-to-many relationships,
           // they will be added independently as post hoc requests.
           waitFor.push(reqId);
-          const id = `{{${reqId}.body@$.id}}`;
-          const type = path([reqId, 'type'], ready);
-          resolved[field] = { id, type };
-        } else if (resolvedIds.length > 0) {
-          resolved[field] = takeFirstResolved(resolvedIds);
+          const resource = {
+            id: `{{${reqId}.body@$.id}}`,
+            type: path([reqId, 'type'], ready),
+          };
+          resolved[field] = resource;
+        } else if (resources.length > 0) {
+          const [resource] = resources;
+          resolved[field] = resource;
         } else {
           unresolved.push(field);
         }
       // ONE-TO-MANY RELATIONSHIPS
       } else if (relation === 'array') {
-        // Concurrent dependencies will be added in a separate subrequest.
+        // Concurrent dependencies will be updated in a separate subrequest sent
+        // to the parent entity's relationships enpoint; that subrequest will be
+        // in the same batch, but subsequent to the original subrequest; hence,
+        // they are separated as "posthoc" (ie, "after the fact") dependencies.
         concurrentIds.forEach((reqId) => {
-          const blueprint = posthocBlueprint(reqId, field);
+          const uuid = `{{${requestId}.body@$.data.id}}`;
+          const blueprint = {
+            requestId: `${requestId}.${field}`,
+            // Note the entity's relationship endpoint is used here.
+            uri: `${BASE_URI}/${entity}/${bundle}/${uuid}/relationships/${field}`,
+            waitFor: [requestId],
+            action: 'create',
+            headers,
+            body: JSON.stringify({
+              data: [{
+                id: `{{${reqId}.body@$.data[*].id}}`,
+                type: path([reqId, 'type'], ready),
+              }],
+            }),
+          };
           posthoc.push(blueprint);
         });
         // Only dependencies from prior request batches can be fully resolved.
-        resolved[field] = concatAllResolved(resolvedIds);
+        resolved[field] = resources;
       } else {
         console.warn(`Uknown field in subrequest for ${bundle} ${entity}: ${field}`);
       }
@@ -229,7 +253,7 @@ export default function makeSendWithSubrequest(farm) {
         const props = { ...resolved, ...constants };
         const data = farm[entity].create(props);
         const body = fmtLocalData(entity, data);
-        const uri = `/api/${entity}/${bundle}`;
+        const uri = `${BASE_URI}/${entity}/${bundle}`;
         const current = {
           action, body, headers, requestId, uri, waitFor,
         };
@@ -244,12 +268,10 @@ export default function makeSendWithSubrequest(farm) {
     },
     // $find(filter, prefix, opts) {
     //   const action = 'view';
-    //   const headers = fetchHeaders;
     //   const { $createIfNotFound, $limit, $sort } = opts;
     // },
     // $update(fields, prefix, opts) {
     //   const action = 'update';
-    //   const headers = headers;
     //   const { $limit, $sort } = opts;
     //   const dependencies = [];
     // },
@@ -260,32 +282,25 @@ export default function makeSendWithSubrequest(farm) {
     let opts = { ...options, ...Object.fromEntries(rest) };
     if (k in actions) return actions[k](v, prefix, opts);
     if (rest.length === 0) {
+      const joinedOpts = Object.keys(options).join(', ');
+      const joinedActs = Object.keys(actions).join(', ');
       const msg = `Missing or invalid action operator in subrequest at ${prefix}. `
-      + `Only the following options or keys were included: ${Object.keys(options).join(', ')}. `
-      + `Include one of the following valid actions instead: ${Object.keys(actions).join(', ')}.`;
+      + `Only the following options or keys were included: ${joinedOpts}. `
+      + `Include one of the following valid actions instead: ${joinedActs}.`;
       throw new Error(msg);
     }
     if (isKeyword(k)) opts = { ...opts, [k]: v };
     return parseSubrequest(Object.fromEntries(rest), opts, prefix);
   }
 
-  // Merge the request data with the response data and parse the body's JSON.
-  function concatSubresponses(response, concurrent = {}, prior = {}) {
-    const merged = mergeWith(mergeLeft, response.data, concurrent);
-    const subresponses = map(evolve({ body: JSON.parse }), merged);
-    return { ...prior, ...subresponses };
-  }
-
   function chainRequests(prior, requests, priority = 0) {
     const [ready, waiting] = partition(r => r.priority === priority, requests);
     const resolveBlueprint = ({ blueprint }) => blueprint(ready, prior, waiting);
     const data = chain(resolveBlueprint, Object.values(ready));
-    const promise = farm.remote.request(SUB_URL, { method: 'POST', data });
+    const promise = farm.remote.request(SUB_URL, { method: 'POST', data })
+      .then(concatSubresponses(prior, ready));
     if (Object.keys(waiting).length === 0) return promise;
-    return promise.then((response) => {
-      const subresponses = concatSubresponses(response, ready, prior);
-      return chainRequests(subresponses, waiting, priority + 1);
-    });
+    return promise.then(done => chainRequests(done, waiting, priority + 1));
   }
 
   return function sendWithSubrequest(data, subrequest = {}) {
