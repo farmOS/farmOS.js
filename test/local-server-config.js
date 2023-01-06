432a61dc664d0524e1d7d5e2cfe65b35ba00f1ec
Jamie Gaehring <gaehj457@gmail.com>
2023-01-06 13:41:26 -0500

    Use host and clientId vars from .env file.

--- test/local-server-config.js
+++ test/local-server-config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 // These are the parameters required to connect to the local server used by
 // tests and scripts in both the development and CI environments.
 module.exports = {
-  host: 'http://localhost',
-  clientId: 'fieldkit',
+  host: process.env.TEST_HOST || 'http://localhost',
+  clientId: process.env.TEST_CLIENT_ID || 'fieldkit',
   username: process.env.TEST_USERNAME || 'admin',
-  password: process.env.TEST_USERNAME || 'admin',
+  password: process.env.TEST_PASSWORD || 'admin',
 };
