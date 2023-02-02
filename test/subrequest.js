f78396353f655c02d492bd6127623472ac91fd42
Jamie Gaehring <gaehj457@gmail.com>
2023-02-02 18:30:51 -0500

    Cleanup subrequest data in test hooks.

--- test/subrequest.js
+++ test/subrequest.js
@@ -14,7 +14,30 @@ describe('subrequest', function () {
   const farm = farmOS({ remote });
   const session = farm.remote.authorize(username, password);
 
-  this.beforeAll(() => session.then(() => farm.schema.fetch()).then(farm.schema.set));
+  const fetchAll = () => Promise.all([
+    farm.log.fetch({ filter: { type: 'log--input', name: 'west field bed 12' } }),
+    farm.quantity.fetch({ filter: { type: 'quantity--standard', label: ['fff', 'ggg', 'hhh'] } }),
+    farm.asset.fetch({ filter: { type: 'asset--land', name: 'west field bed 12' } }),
+    farm.term.fetch({ filter: { type: 'taxonomy_term--log_category', name: 'pest_disease_control' } }),
+    farm.term.fetch({ filter: { type: 'taxonomy_term--unit', name: ['US_gal', 'US_gal_acre'] } }),
+  ]);
+
+  const cleanup = (shortName, bundle, response) =>
+    response.data.map(d => farm[shortName].delete(bundle, d.id));
+  const cleanupAll = ([logs, quantities, locations, categories, units]) => Promise.all([
+    cleanup('log', 'input', logs),
+    cleanup('quantity', 'standard', quantities),
+    cleanup('asset', 'land', locations),
+  ].flat()).then(() => Promise.all([
+    // These terms can only be deleted AFTER the entities referencing them have been.
+    cleanup('term', 'log_category', categories),
+    cleanup('term', 'unit', units),
+  ].flat()));
+
+  this.beforeAll(() => session
+    .then(() => farm.schema.fetch()).then(farm.schema.set)
+    .then(() => fetchAll().then(cleanupAll))
+    .catch(reportError));
 
   it('Send both a quantity and a log with subrequests', () => session.then(() => {
     const quantF = farm.quantity.create({
@@ -124,4 +147,6 @@ describe('subrequest', function () {
     expect(log).to.have.nested.property('relationships.quantity')
       .that.has.a.lengthOf(3);
   }).catch(reportError));
+
+  this.afterAll(() => fetchAll().then(cleanupAll).then(reportError));
 });
